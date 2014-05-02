425915463c242394121fe0d3b17d025eb5ec1d47
Obed N Munoz <obed.n.munoz@gmail.com>
2014-05-01 19:09:29 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,51 @@
+"""
+
+"""
+
+__author__ = 'craig'
+import pygame
+import json_loader
+
+
+
+def main():
+
+    # initialize pygame
+    pygame.init()
+    # set screen size of phone or desktop window.  Adjust to your phone
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Use keyboard arrow keys or mouse.  Press TEST to toggle overlay")
+    
+
+    # game framerate.  Higher number is faster
+    FPS = 40
+
+    # change the file names to your player graphic and map file
+    player_image_file = "img/boy.png"
+    map_file = "maps/mega_map.json"
+
+    # change to False (with capital F) to turn off red squares over
+    # collision rectangles
+    TESTING = False
+
+    #initialize json loader, build tileset list, load player graphic
+    initial = json_loader.Initialize(screen, TESTING, map_file, player_image_file)
+
+    # initialize position of player when game first starts
+    map = json_loader.Map(initial)
+    map.move(-200, 0)
+
+    # handle events such as keyboard / touchscreen presses
+    event = json_loader.Event(initial)
+    clock = pygame.time.Clock()
+
+
+    while True:
+        event.update()
+        map.update(event.direction)
+        map.display(screen)
+        clock.tick(FPS)
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
